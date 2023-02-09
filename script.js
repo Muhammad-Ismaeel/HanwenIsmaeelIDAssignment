6eba84a6d192db3fd6e5d0498b2632573bab28a0
Muhammad-Ismaeel <s10219144@connect.np.edu.sg>
2023-02-09 17:07:16 +0800

    api thing kinda solved

---
+++ script.js
@@ -0,0 +1,19 @@
+let url ="https://api.apify.com/v2/key-value-stores/tVaYRsPHLjNdNBu7S/records/LATEST?disableRedirect=true"
+fetch(url)
+.then(response => response.json())
+.then(function(response){
+    for(let i = 0; i< response.length; i++){
+        let cases = (response[i]["infected"] - response[i]["deceased"] - response[i]["recovered"])
+        if(response[i]["country"] != "Singapore" && cases > 0 && cases != null){
+            let countrysentence = "Country: " + response[i]["country"]
+            let casessentence = "Current cases: " + cases
+            let newdivone = document.createElement("div")
+            newdivone.innerHTML = countrysentence
+            document.querySelector(".countrytable").append(newdivone)
+            let newdivtwo = document.createElement("div")
+            newdivtwo.innerHTML = casessentence
+            document.querySelector(".casestable").append(newdivtwo)
+        }
+        
+    }
+})
